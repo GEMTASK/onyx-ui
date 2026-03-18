0915e828ec623323d2ae18b966c71d9b25eabd6e
Mike Austin <mike_ekim@yahoo.com>
2026-03-17 17:45:59 -0700

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
+import { esmExternalRequirePlugin } from 'rolldown/plugins';
+import dts from 'vite-plugin-dts';
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.tsx'),
+      name: 'Onyx UI',
+      fileName: 'main'
+    },
+    rolldownOptions: {
+      // external: [
+      //   'react',
+      //   'react-dom'
+      // ]
+      plugins: [
+        esmExternalRequirePlugin({
+          external: ['react', 'react-dom'],
+        }),
+      ]
+    }
+  },
+  plugins: [
+    react(),
+    babel({
+      presets: [reactCompilerPreset()]
+    }),
+    dts({
+      tsconfigPath: './tsconfig.app.json'
+    })
+  ],
+});
